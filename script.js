d8400c27c71c13f77ac30d8705f7813b6226b9f7
dd0353 <dd0353@srmist.edu.in>
2026-04-26 16:53:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
+console.log("JS FILE LOADED");
 class Vehicle {
-    constructor(number) {
+    constructor(number, type) {
         this.number = number;
+        this.type = type;
     }
+
     calculateFee(hours) { return 0; }
 }
 
 class Car extends Vehicle {
-    calculateFee(hours) { return hours * 20; }
+    constructor(number) {
+        super(number, "Car");
+    }
+
+    calculateFee(hours) {
+        return hours * 20;
+    }
 }
 
 class Bike extends Vehicle {
-    calculateFee(hours) { return hours * 10; }
+    constructor(number) {
+        super(number, "Bike");
+    }
+
+    calculateFee(hours) {
+        return hours * 10;
+    }
 }
 
+// Parking data
 let vehicles = [];
+let capacity = 10;
 
+// Park Car
 function parkCar() {
     let num = document.getElementById("vehicleNumber").value;
+
+    if (vehicles.length >= capacity) {
+        document.getElementById("output").innerText = "Parking FULL!";
+        return;
+    }
+
     vehicles.push(new Car(num));
     document.getElementById("output").innerText = "Car Parked!";
 }
 
+// Park Bike
 function parkBike() {
     let num = document.getElementById("vehicleNumber").value;
+
+    if (vehicles.length >= capacity) {
+        document.getElementById("output").innerText = "Parking FULL!";
+        return;
+    }
+
     vehicles.push(new Bike(num));
     document.getElementById("output").innerText = "Bike Parked!";
 }
 
+// Remove Vehicle
 function removeVehicle() {
     if (vehicles.length === 0) {
         document.getElementById("output").innerText = "No vehicles!";
@@ -37,5 +69,26 @@ function removeVehicle() {
     let hours = prompt("Enter hours:");
     let fee = v.calculateFee(hours);
 
-    document.getElementById("output").innerText = "Fee: " + fee;
+    document.getElementById("output").innerText =
+        v.type + " removed. Fee: " + fee;
+}
+
+// Show Vehicles
+function showVehicles() {
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+
+    vehicles.forEach(v => {
+        let li = document.createElement("li");
+        li.innerText = v.type + " - " + v.number;
+        list.appendChild(li);
+    });
+}
+
+// Show Status
+function showStatus() {
+    document.getElementById("status").innerText =
+        "Total: " + capacity +
+        " | Parked: " + vehicles.length +
+        " | Available: " + (capacity - vehicles.length);
 }
